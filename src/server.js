ecb7c40b7b8a69b481bdc9203d4664c9060f033d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-07 14:59:39 +0900

    미들웨어 개념 이해

--- src/server.js
+++ src/server.js
@@ -4,6 +4,34 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+};
+
+const privateMiddleware = (req, res, next) => {
+    const url = req.url;
+    if (url === "/protected") {
+        return res.send("<h1>Not Allowed</h1>")
+    }
+    console.log("Allowed, you may continue");
+    next();
+};
+
+const handleHome = (req, res, next) => {
+    return res.send("I love middleware");
+};
+
+const handleProtected = (req, res) => {
+    return res.send("Welcome to thr private lounge");
+};
+
+// use: 모든 route에서 사용하는 미들웨어
+app.use(logger)
+app.use(privateMiddleware)
+app.get("/", handleHome);
+app.get("/protected", handleProtected);
+
 const handleListneing = () => console.log(`Server listening on port http://localhost:${PORT}🚀`);
 
 app.listen(PORT, handleListneing)
