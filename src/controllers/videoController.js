d780c067f857145fbb89e596e22cdf0a5c6c5480
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-14 22:40:16 +0900

    watch 파일 생성

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,5 @@
 export const trending =(req, res) => res.render("home");
-export const see =(req, res) => {
-    console.log(req.params);
-    return res.send(`Watch Video #${req.params.id}`);
-}
+export const see =(req, res) => res.render("watch");
 export const edit =(req, res) => res.send("Edit");
 export const search =(req, res) => res.send("Search");
 export const upload =(req, res) => res.send("Upload");
