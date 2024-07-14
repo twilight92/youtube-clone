346bff683f4b13d3f39ea386a77e999dd813c71d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-14 23:51:42 +0900

    fakeUser 제거/Iteration

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,7 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home", fakeUser });
+export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return res.render("home", { pageTitle: "Home", videos });
+}
 export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
