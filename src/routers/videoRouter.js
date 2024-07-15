6ba3b2cbf32b65d0d9ad4efa5424a60803cbd44b
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-15 23:45:17 +0900

    불필요한 코드 제거

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,7 +6,5 @@ const videoRouter = express.Router();
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", getEdit);
 videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
-videoRouter.get("/upload", upload);
 
 export default videoRouter;
