cfc0a26f3ee0f82a57f5a52de458259e353a33af
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-20 21:02:01 +0900

    . → , 로 변경/ string upppercase 적용

--- src/models/Video.js
+++ src/models/Video.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: { type: String. required: true },
-    description: { type: String. required: true },
+    title: { type: String, required: true, uppercase: true },
+    description: { type: String, required: true },
     createdAt: { type: Date, required: true, default: Date.now },
     hashtags: [{ type: String }],
     meta: {
