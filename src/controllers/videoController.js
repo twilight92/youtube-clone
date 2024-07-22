b133a817e6ac15040cdf716053e9a01d7bae2496
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-23 00:11:25 +0900

    middlewares를 save 이벤트에 적용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -42,7 +42,7 @@ export const postEdit = async (req, res) => {
     await Video.findByIdAndUpdate(video, {
         title,
         description,
-        hashtags: hashtags.split(",").map(word => word.startsWith('#') ? word : `#${word}`)
+        hashtags
     })
 
     await video.save();
@@ -57,7 +57,7 @@ export const postUpload = async (req, res) => {
         await Video.create({
             title,
             description,
-            hashtags: hashtags.split(",").map(word => `#${word}`),
+            hashtags,
         });
         return res.redirect("/");
     } catch(error) {
