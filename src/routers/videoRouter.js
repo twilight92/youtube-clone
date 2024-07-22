f79457981b23b1f2cac14b7ea1b5f348cc072177
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-22 20:59:07 +0900

    /upload가 :id에 해당하지 않도록 상단으로 위치 이동

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 
 const videoRouter = express.Router();
 
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id", watch);
 videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
-videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
