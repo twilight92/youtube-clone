743cc70c018d674478fc884f2c216c3963b56fdf
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-22 20:57:15 +0900

    mongoDB가 만들어낸 id 포맷과 digit이 맞지 않아 제거

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 
 const videoRouter = express.Router();
 
-videoRouter.get("/:id(\\d+)", watch);
-videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id", watch);
+videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
