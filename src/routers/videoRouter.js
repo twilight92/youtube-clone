bf3ecae4e99ce057116cb1ab2eaf66f333893bc3
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-22 21:07:15 +0900

    mongoDB가 만들어낸 id 포맷과 맞춤(24자 16진수 문자열 값 0-9, a-f )

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,7 +4,7 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 const videoRouter = express.Router();
 
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id", watch);
-videoRouter.route("/:id/edit").get(getEdit).post(postEdit);
+videoRouter.get("/:id([0-9a-f]{24})", watch);
+videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 
 export default videoRouter;
