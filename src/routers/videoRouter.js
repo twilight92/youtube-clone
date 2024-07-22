a8f6c6ad7477f557a88647df52b93afa7ca8e226
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-22 21:53:27 +0900

    db video 연결

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -3,8 +3,8 @@ import { watch, getEdit, postEdit, getUpload, postUpload } from "../controllers/
 
 const videoRouter = express.Router();
 
-videoRouter.route("/upload").get(getUpload).post(postUpload);
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
 export default videoRouter;
