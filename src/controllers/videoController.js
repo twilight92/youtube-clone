2dec4fed1b7985f417ce012381061521f8f375f2
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-30 23:27:20 +0900

    불필요한 콘솔 제거

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -85,7 +85,6 @@ export const search = async(req, res) => {
                 $regex: new RegExp(keyword, "i")
             }
         });
-        console.log(videos)
     }
 
     return res.render("search", { pageTitle: "Search", videos });
