daf8bb0187ee4b98bcadb812706f364356f7a30d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-31 23:06:13 +0900

    불필요한 콘솔 제거

--- src/models/User.js
+++ src/models/User.js
@@ -10,9 +10,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.pre("save", async function() {
-    console.log("users password", this.password)
     this.password = await bcrypt.hash(this.password, 5);
-    console.log("hashed password", this.password)
 });
 
 const User = mongoose.model("User", userSchema);
