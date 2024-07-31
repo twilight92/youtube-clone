581c0788b2c22014444637a2eb3415349bb82c76
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-31 23:42:07 +0900

    상태 코드 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -39,7 +39,7 @@ export const postEdit = async (req, res) => {
     const video = await Video.exists({ _id: id });
 
     if (!video) {
-        return res.render("404", { pageTitle: "Video not found"});
+        return res.status(404).render("404", { pageTitle: "Video not found"});
     }
 
     await Video.findByIdAndUpdate(video, {
