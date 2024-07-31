56410a879d31e506c712a08632d464b555ea384f
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-31 23:20:23 +0900

    unique: true가 중복 생성일 때 대응

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,6 +3,25 @@ import User from "../models/User";
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
     const {name, username, email, password, location} = req.body;
+    const pageTitle = "Join";
+    const usernameExists = await User.exists({ username });
+
+    if (usernameExists) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "This username is already taken."
+        });
+    }
+    
+    const emailExists = await User.exists({ email });
+
+    if (emailExists) {
+        return res.render("join", {
+            pageTitle,
+            errorMessage: "This email is already taken."
+        });
+    }
+
     await User.create({
         name, username, email, password, location,
     });
