55674c8d09234b2677a468ef644de3882793c422
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-07-31 23:40:37 +0900

    상태 코드 추가(브라우저에게 이 URL을 저장하지 말라고 알려줌 = 알맞은 상태 코드를 보내는 것이 유저들에게 좋은 이유)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -27,7 +27,7 @@ export const getEdit = async (req, res) => {
     const video = await Video.findById(id);
 
     if (!video) {
-        return res.render("404", { pageTitle: "Video not found"});
+        return res.status(404).render("404", { pageTitle: "Video not found"});
     }
 
     return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
@@ -65,7 +65,7 @@ export const postUpload = async (req, res) => {
         });
         return res.redirect("/");
     } catch(error) {
-        return res.render("upload", { pageTitle: "Upload Video", errorMessage: error._message });
+        return res.status(400).render("upload", { pageTitle: "Upload Video", errorMessage: error._message });
     }
 };
 
