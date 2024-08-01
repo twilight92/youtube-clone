7da9f4fbc1e689c8cbc9f4dfda31943903e7a644
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-01 22:35:00 +0900

    로그인 함수명 변경

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -32,6 +32,6 @@ export const postJoin = async (req, res) => {
 };
 export const edit = (req, res) => res.render("edit");
 export const remove = (req, res) => res.send("Remove Usere");
-export const login = (req, res) => res.send("Login");
+export const getLogin = (req, res) => res.send("Login");
 export const logout = (req, res) => res.send("Log out");
 export const see = (req, res) => res.send("See User");
