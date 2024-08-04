6a76137e188780ebf722d4a95d1d6deec4d6c6ac
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-04 17:51:12 +0900

    변경사항이 없음에도 세션을 저장(true)하면 비효율적이므로 효율을 높이기 위해 사용

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
     secret: "Hello",
-    resave: true,
+    resave: false,
     saveUninitialized: true,
     store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/wetube' }),
 }));
