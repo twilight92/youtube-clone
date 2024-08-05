aea9c7c48eec4a1b1f2374f0c87b08337af78c4c
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-05 19:56:55 +0900

    따옴표 변경

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -64,7 +64,7 @@ export const postLogin = async(req, res) => {
 }
 
 export const startGithubLogin = (req, res) => {
-    const baseURL = `https://github.com/login/oauth/authorize`;
+    const baseURL = "https://github.com/login/oauth/authorize";
     const config = {
         client_id: process.env.GH_CLIENT,
         allow_signup: false,
