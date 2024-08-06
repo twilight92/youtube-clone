84a6f0a3aceca4b122b5636409118ef50b61000b
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-07 00:00:41 +0900

    로그인하지 않은 사람들이 우리가 보호하려는 페이지에 가는 것 막기(user가 loggedIn 돼 있다면 요청 계속, 그렇지 않으면 로그인 페이지로 redirect)

--- src/middlewares.js
+++ src/middlewares.js
@@ -6,3 +6,11 @@ export const localsMiddleware = (req, res, next) => {
 
     next();
 };
+
+export const protectorMiddleware = (req, res, next) => {
+    if (req.session.loggedIn) {
+        next();
+    } else {
+        return res.redirect("/login");
+    }
+};
