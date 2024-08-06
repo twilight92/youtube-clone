8736afcd631ab58d8d74d4f29a462a3047b2f9cd
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-06 17:45:28 +0900

    edit profile get, post 분리

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -140,7 +140,13 @@ export const finishGithubLogin = async (req, res) => {
     }
 };
 
-export const edit = (req, res) => res.render("Edit User");
+export const getEdit = (req, res) => {
+    return res.render("edit-profile", { pageTitle: "Edit Profile" });
+}
+export const postEdit = (req, res) => {
+    return res.render("edit-profile");
+}
+
 export const logout = (req, res) => {
     req.session.destroy();
     return res.redirect("/");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { edit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController";
+import { getEdit, postEdit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
-userRouter.get("/edit", edit);
+userRouter.route("/edit").get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
