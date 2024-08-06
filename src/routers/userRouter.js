2949880490e7b953fe089bce071bf4bd19484de7
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-07 00:06:04 +0900

    all()을 활용한 반복 제거(get, post 등 어떤 http method를 사용하든지 이 미들웨어를 사용하겠다는 것)

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -5,7 +5,7 @@ import { protectorMiddleware } from "../middlewares";
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").get(protectorMiddleware, getEdit).post(protectorMiddleware, postEdit);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
