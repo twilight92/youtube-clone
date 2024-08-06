d5e27927753bb72741d59b61674bd54b34087732
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-07 00:04:33 +0900

    로그인 돼 있는 사람들만 로그아웃 페이지로 갈 수 있게 protectorMiddleware 적용

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { getEdit, postEdit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController";
+import { protectorMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").get(protectorMiddleware, getEdit).post(protectorMiddleware, postEdit);
 userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get("/:id", see);
