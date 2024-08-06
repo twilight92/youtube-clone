388468d0a61f572f8636be2d248175c128442527
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-07 00:07:16 +0900

    로그아웃 돼 있어야 실행시키는 것을 허락해주는 publicOnlyMiddleware 미들웨어 적용

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,13 +1,13 @@
 import express from "express";
 import { getEdit, postEdit, logout, startGithubLogin, finishGithubLogin, see } from "../controllers/userController";
-import { protectorMiddleware } from "../middlewares";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", protectorMiddleware, logout);
 userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
