417b7b452fb731b51e07e47422a8632165ff3401
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-08 22:29:47 +0900

    edit-profile form에서 파일을 보낼 때도, 아닐 때도 있기 때문에 조건문 사용

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -146,7 +146,7 @@ export const getEdit = (req, res) => {
 export const postEdit = async (req, res) => {
     const {
         session: {
-            user: {_id}
+            user: { _id, avatarUrl }
         },
         body: { name, email, username, location },
         file
@@ -157,6 +157,7 @@ export const postEdit = async (req, res) => {
     console.log(file);
 
     const updatedUser = await User.findByIdAndUpdate(_id, {
+        avatarUrl: file ? file.path : avatarUrl,
         name,
         email,
         username,
