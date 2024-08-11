e0a37b273d69e875618c9105eaa16cd07479296d
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-11 23:41:50 +0900

    babel-loader 적용

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,20 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "assets", "js")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ],
+                    }
+                }
+            }
+        ]
     }
 };
