06f7c8adf2bc6d45135edf1f81099b5d91724567
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-11 23:46:37 +0900

    개발 모드로 mode 변경 - mode를 설정해주지 않으면 webpack은 기본으로 production mode로 설정하는데 개발 중에 코드를 압축하면 어떻게 코드를 짜고 있는지 알 수 없어서 develop mode가 필요하다(Set 'mode' option to 'development' or 'production')

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path")
 
 module.exports = {
     entry: './src/client/js/main.js',
+    mode: 'development',
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "assets", "js")
