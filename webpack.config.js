327c0ef49bf488ad4680c381c1e275addafa2624
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-13 20:52:26 +0900

    제일 마지막 loader부터 역순으로 시작해야한다(웹팩은 뒤에서부터 시작/일반적인 css로 변경 → css-loader에 전달 → style을 css를 브라우저에 보이게 하는 것 )

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
                         ],
                     }
                 }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'scss-loader']
             }
         ]
     }
