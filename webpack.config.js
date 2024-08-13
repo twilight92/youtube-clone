4766fca9f336191b08b8f9349796a808cf882f09
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-13 21:10:44 +0900

    MiniCssExtractPlugin이 생성된 css 파일을 ouput과 같은 위치에 두지 않도록 변경 (assets에 두 개의 분리된 파일이 생성됨)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     mode: 'development',
     output: {
         filename: "main.js",
-        path: path.resolve(__dirname, "assets", "js")
+        path: path.resolve(__dirname, "assets")
     },
     module: {
         rules: [
