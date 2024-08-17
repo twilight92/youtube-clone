e36cc740c827e4514680b4029466f6a2f1826259
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-18 00:46:18 +0900

    mouse가 video로 들어오면 컨트롤러를 노출하고 떠나면 숨기는 기능 추가(이 때 mouseleave시 실행되는 setTimeout을 clearTimeout 해주지 않으면 mouseleave 즉시 mousemove가 일어나더라도 showing 클래스가 제거되는 이슈가 있다.)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -9,7 +9,9 @@ const volumeRange = document.getElementById("volume");
 const timelineRange = document.getElementById("timeline");
 const fullScreenBtn = document.getElementById("fullScreen");
 const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -82,6 +84,21 @@ const handleFullScreen = () => {
   }
 };
 
+const handleMouseMove = () => {
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+
+  videoControls.classList.add("showing");
+};
+
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(() => {
+    videoControls.classList.remove("showing");
+  }, 3000);
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
@@ -89,6 +106,8 @@ video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timelineRange.addEventListener("input", handleTimelineChange);
 fullScreenBtn.addEventListener("click", handleFullScreen);
+video.addEventListener("mousemove", handleMouseMove);
+video.addEventListener("mouseleave", handleMouseLeave);
 
 window.addEventListener("keydown", (event) => {
   if (event.code == "Enter") {
