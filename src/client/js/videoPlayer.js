4be311234a1492e58cf643803d181c9fb45747de
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-17 21:31:25 +0900

    비디오 정지/재생 기능 추가

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,27 @@
 console.log("video");
+
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    // if the video is playing, pause it
+    video.play();
+  } else {
+    // else play the video
+    video.pause();
+  }
+};
+
+const handleMute = (e) => {};
+
+const handlePause = (e) => (playBtn.innerText = "Play");
+const handlePlay = (e) => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
