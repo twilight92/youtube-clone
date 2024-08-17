38a29c762f0ccb54cd533b358606b09d7b1bbd5e
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-18 01:00:26 +0900

    마우스가 3초간 멈추면 컨트롤러 숨기는 기능(mousestop이 없어서 setTimeout과 clearTimeout을 사용해야한다.)

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -12,6 +12,7 @@ const videoContainer = document.getElementById("videoContainer");
 const videoControls = document.getElementById("videoControls");
 
 let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
 
@@ -84,19 +85,25 @@ const handleFullScreen = () => {
   }
 };
 
+const hideControls = () => videoControls.classList.remove("showing");
+
 const handleMouseMove = () => {
   if (controlsTimeout) {
     clearTimeout(controlsTimeout);
     controlsTimeout = null;
   }
 
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+
   videoControls.classList.add("showing");
+  controlsMovementTimeout = setTimeout(hideControls, 3000);
 };
 
 const handleMouseLeave = () => {
-  controlsTimeout = setTimeout(() => {
-    videoControls.classList.remove("showing");
-  }, 3000);
+  controlsTimeout = setTimeout(hideControls, 3000);
 };
 
 playBtn.addEventListener("click", handlePlayClick);
