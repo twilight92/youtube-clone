8502a087b2a64073e205eb593ce04caadfbfbe63
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-18 22:17:46 +0900

    녹화 전 미리보기 화면 추가/stop, start 기능 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,7 +1,19 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+};
+
+const init = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
@@ -10,4 +22,6 @@ const handleStart = async () => {
   video.srcObject = stream;
   video.play();
 };
+
+init();
 startBtn.addEventListener("click", handleStart);
