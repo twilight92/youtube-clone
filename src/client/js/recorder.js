e2342901dc488dcb1d757dcfa89097121beec381
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-18 23:07:25 +0900

    ondataavailable 이벤트와 createObjectURL을 활용하여 녹화된 비디오 확인하는 기능 추가

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,14 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 let stream;
+let recorder;
 
 const handleStop = () => {
   startBtn.innerText = "Start Recording";
   startBtn.removeEventListener("click", handleStop);
   startBtn.addEventListener("click", handleStart);
+
+  recorder.stop();
 };
 
 const handleStart = () => {
@@ -13,24 +16,21 @@ const handleStart = () => {
   startBtn.removeEventListener("click", handleStart);
   startBtn.addEventListener("click", handleStop);
 
-  const recorder = new MediaRecorder(stream);
-  recorder.ondataavailable = (e) => {
-    console.log("recording done");
-    console.log("e.data", e.data);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    const videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
   };
-  console.log("start() 전", recorder);
   recorder.start();
-  console.log("start() 후", recorder);
-
-  setTimeout(() => {
-    recorder.stop();
-  }, 10000);
 };
 
 const init = async () => {
   stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
-    video: { width: 300, height: 100 },
+    video: true,
   });
 
   video.srcObject = stream;
