e285c69238dd723aa55430ba3cc41a9686191379
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-19 23:04:00 +0900

    FFmpeg를 활용한 Transcode Video(webm → mp4)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,22 @@
+import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 let stream;
 let recorder;
 let videoFile;
 
-const handleDownload = () => {
+const handleDownload = async () => {
+  const ffmpeg = createFFmpeg({ log: true });
+  // await을 사용하는 이유는 사용자가 소프트웨어를 사용할 것이기 때문(사용자가 무언가를 설치해서 javascript가 아닌 코드를 사용하는 것)
+  await ffmpeg.load();
+
+  // WebAssembly를 사용하기 때문에 더이상 브라우저에 있는 것이 아니다. (ffmpeg 가상의 세계에 파일 생성 가능)
+  // FS는 FileSystem writeFile 뒤 인자는 파일명, 그 다음은 binaryData(영상 정보를 가리키는 URL = videoFile) function
+  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+
+  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+
   const a = document.createElement("a");
   a.href = videoFile;
   a.download = "MyRecoding.webm";
