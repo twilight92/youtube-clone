57a6a1e42cfd4050e03d547f3f2f4251e932d405
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-20 22:40:25 +0900

    Uncaught (in promise) ReferenceError: SharedArrayBuffer is not defined 에러 해결(SharedArrayBuffer는 cross-origin isolated된 페이지에서만 사용할 수 있습니다. 따라서 ffmpeg.wasm을 사용하려면 Cross-Origin-Embedder-Policy: require-corp 및 Cross-Origin-Opener-Policy: same-origin를 header에 설정해 자체 서버를 호스팅해야 합니다.)

--- src/server.js
+++ src/server.js
@@ -13,6 +13,14 @@ const logger = morgan("dev");
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+
+// ffmpeg.wasm을 사용하려면 Cross-Origin-Embedder-Policy: require-corp 및 Cross-Origin-Opener-Policy: same-origin를 header에 설정해 자체 서버를 호스팅해야 함/라우터 앞에 추가해야 함
+app.use((req, res, next) => {
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  next();
+});
+
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
