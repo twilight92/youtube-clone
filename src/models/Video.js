cf2fd4d8a1eb4f6f6f3a4540c646750c88f91001
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-26 21:19:18 +0900

    프리티어 적용 및 관계

--- src/models/Video.js
+++ src/models/Video.js
@@ -17,6 +17,9 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, required: true, default: 0 },
     rating: { type: Number, required: true, default: 0 },
   },
+  commments: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+  ],
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
