48fd6928757c5ed506a31714fcf3985aeab2c00e
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-28 20:57:09 +0900

    실시간 댓글처럼 보이도록 reload()

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,7 +1,7 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = (event) => {
+const handleSubmit = async (event) => {
   event.preventDefault();
 
   const textarea = form.querySelector("textarea");
@@ -12,7 +12,7 @@ const handleSubmit = (event) => {
     return;
   }
 
-  fetch(`/api/videos/${videoId}/comment`, {
+  await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,6 +21,7 @@ const handleSubmit = (event) => {
   });
 
   textarea.value = "";
+  window.location.reload();
 };
 
 if (form) {
