995c67180f03ff46fb8f73ca08a6b09ab749728e
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-28 20:46:41 +0900

    댓글 생성 후 비디오 업데이트

--- src/models/Video.js
+++ src/models/Video.js
@@ -17,7 +17,7 @@ const videoSchema = new mongoose.Schema({
     views: { type: Number, required: true, default: 0 },
     rating: { type: Number, required: true, default: 0 },
   },
-  commments: [
+  comments: [
     { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
   ],
   owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
