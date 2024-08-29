3e16d5da100d62cf1954d35b213015499eeaba76
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-08-29 21:32:11 +0900

    실시간 댓글 구현 - 새로고침 하지 않기 위해 pug가 아닌 js로 댓글 생성

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,19 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
 
@@ -12,7 +25,7 @@ const handleSubmit = async (event) => {
     return;
   }
 
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,6 +34,9 @@ const handleSubmit = async (event) => {
   });
 
   textarea.value = "";
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
