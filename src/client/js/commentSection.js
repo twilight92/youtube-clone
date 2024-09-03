02aa725eb6125cf505f54e89710669c523a0b903
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-03 20:43:18 +0900

    댓글 삭제

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,5 +1,6 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
+const deleteIcon = document.querySelectorAll(".delete__icon");
 
 const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
@@ -8,6 +9,7 @@ const addComment = (text, id) => {
   newComment.className = "video__comment";
   const icon = document.createElement("i");
   icon.className = "fas fa-comment";
+  deleteIcon.className = "delete__icon";
   const span = document.createElement("span");
   span.innerText = ` ${text}`;
   const span2 = document.createElement("span");
@@ -16,6 +18,7 @@ const addComment = (text, id) => {
   newComment.appendChild(span);
   newComment.appendChild(span2);
   videoComments.prepend(newComment);
+  deleteIcon.addEventListener("click", handleDelete);
 };
 
 const handleSubmit = async (event) => {
@@ -44,6 +47,32 @@ const handleSubmit = async (event) => {
   }
 };
 
+const handleDelete = async (event) => {
+  const deleteComment = event.target.parentElement;
+
+  const {
+    dataset: { id },
+  } = event.target.parentElement;
+
+  const videoId = videoContainer.dataset.id;
+
+  const response = await fetch(`/api/videos/${videoId}/comment/delete`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ commentId: id }),
+  });
+
+  if (response.status === 200) {
+    deleteComment.remove();
+  }
+};
+
 if (form) {
   form.addEventListener("submit", handleSubmit);
+}
+
+if (deleteIcon) {
+  deleteIcon.forEach((icon) => icon.addEventListener("click", handleDelete));
 }
