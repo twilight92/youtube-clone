c825a629239064d60af134ba795fe6c0d0b80832
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-05 21:20:19 +0900

    로직은 같으나 API, 사용하는 함수가 달라진 부분 수정(패키지 제작자가 함수 이름을 변경)

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,4 +1,5 @@
-import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 
 const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
@@ -27,30 +28,30 @@ const handleDownload = async () => {
 
   actionBtn.disabled = true;
 
-  const ffmpeg = createFFmpeg({ log: true });
+  const ffmpeg = new FFmpeg();
   // await을 사용하는 이유는 사용자가 소프트웨어를 사용할 것이기 때문(사용자가 무언가를 설치해서 javascript가 아닌 코드를 사용하는 것)
   await ffmpeg.load();
 
   // WebAssembly를 사용하기 때문에 더이상 브라우저에 있는 것이 아니다. (ffmpeg 가상의 세계에 파일 생성 가능)
   // FS는 FileSystem writeFile 뒤 인자는 파일명, 그 다음은 binaryData(영상 정보를 가리키는 URL = videoFile) function
-  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
+  await ffmpeg.writeFile(files.input, await fetchFile(videoFile));
 
   // 브라우저의 메모리에는 output.mp4 파일이 있다.
-  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
+  await ffmpeg.exec(["-i", files.input, "-r", "60", files.output]);
 
-  await ffmpeg.run(
+  await ffmpeg.exec([
     "-i",
     files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    files.thumb
-  );
+    files.thumb,
+  ]);
 
   // ffmpeg의 FS(파일 시스템)을 이용해서 mp4, jpg 파일을 가져온다.
-  const mp4File = ffmpeg.FS("readFile", files.output);
-  const thumbFile = ffmpeg.FS("readFile", files.thumb);
+  const mp4File = await ffmpeg.readFile(files.output);
+  const thumbFile = await ffmpeg.readFile(files.thumb);
 
   /* 
     // unit8Array 타입 - 삭제 or 파일 합치기 등 뭐든 할 수 있는 원시 파일
@@ -73,9 +74,9 @@ const handleDownload = async () => {
   downloadFile(thumbUrl, "MyThumbnail.jpg");
 
   // 속도 개선을 위해 파일 링크 해제
-  ffmpeg.FS("unlink", files.input);
-  ffmpeg.FS("unlink", files.output);
-  ffmpeg.FS("unlink", files.thumb);
+  await ffmpeg.deleteFile(files.input);
+  await ffmpeg.deleteFile(files.output);
+  await ffmpeg.deleteFile(files.thumb);
 
   // URL도 삭제(revokeObjectURL 이 객체를 메모리에서 지우고 싶다는 것)
   URL.revokeObjectURL(videoFile);
