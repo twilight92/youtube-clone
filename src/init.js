1f871d0b72d45bb3cea23403f49d88057cf8674e
BOOK-D15UEBQLJP\tnwjd <tnwjd2050@naver.com>
2024-09-08 21:36:18 +0900

    포트가 달라 애플리케이션을 찾을 수 없으므로 fly.toml internal_port와 통일

--- src/init.js
+++ src/init.js
@@ -5,7 +5,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = 3000;
 
 const handleListneing = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}🚀`);
